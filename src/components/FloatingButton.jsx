b165c5be05198a5261ca90d1c22f8fbc7e35230b
Chris <cristianortizventas@gmail.com>
2025-07-17 03:11:38 -0500

    CV modification

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
 const FloatingButton = () => {
   const { t, i18n } = useTranslation();
 
-  const cvLink = i18n.language === "es" ? "/cv-Sp.pdf" : "/cv-En.pdf";
+  const cvLink = i18n.language === "es" ? "/cv-Spa.pdf" : "/cv-Eng.pdf";
 
   return (
     <a
