acb747010f3d1d9276930f8b6134f66c79d32dc3
Chris <cristianortizventas@gmail.com>
2025-09-23 21:21:23 -0500

    Modification

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      sm: "350px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
+      sm: '350px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
 
     colors: {
-      brown: "#53423e",
-      lightBrown: "#645550",
-      darkBrown: "#2c2523",
-      black: "#1e1917",
-      white: "#f1e1d9",
-      cyan: "#15d1e9",
-      lightCyan: "#88e5f0",
-      darkCyan: "#009fb3",
-      orange: "#fb9718",
-      lightOrange: "#fac27b",
-      darkOrange: "#d28422",
-      grey: "#626965",
-      lightGrey: "#978580",
-      darkGrey: "#3f4441",
+      brown: '#53423e',
+      lightBrown: '#645550',
+      darkBrown: '#2c2523',
+      black: '#1e1917',
+      white: '#f1e1d9',
+      cyan: '#15d1e9',
+      lightCyan: '#88e5f0',
+      darkCyan: '#009fb3',
+      orange: '#fb9718',
+      lightOrange: '#fac27b',
+      darkOrange: '#d28422',
+      grey: '#626965',
+      lightGrey: '#978580',
+      darkGrey: '#3f4441',
     },
     extend: {
       boxShadow: {
-        cyanShadow: "0px 0px 20px 0px rgba(94, 206, 220, 0.5)",
-        cyanBigShadow: "10px 10px 1000px 500px rgba(94, 206, 220, 0.3)",
-        cyanMediumShadow: "10px 10px 200px 150px rgba(94, 206, 220, 0.5)",
-        orangeBigShadow: "10px 10px 10000px 500px rgba(240, 169, 79, 0.3)",
-        orangeMediumShadow: "10px 10px 2000px 150px rgba(240, 169, 79, 0.5)",
+        cyanShadow: '0px 0px 20px 0px rgba(94, 206, 220, 0.5)',
+        cyanBigShadow: '10px 10px 1000px 500px rgba(94, 206, 220, 0.3)',
+        cyanMediumShadow: '10px 10px 200px 150px rgba(94, 206, 220, 0.5)',
+        orangeBigShadow: '10px 10px 10000px 500px rgba(240, 169, 79, 0.3)',
+        orangeMediumShadow: '10px 10px 2000px 150px rgba(240, 169, 79, 0.5)',
         // Añadimos el text-shadow personalizado
-        "text-glow":
-          "0 0 5px #FFF, 0 0 10px #FFF, 0 0 15px #FFF, 0 0 20px #49ff18, 0 0 30px #49FF18, 0 0 40px #49FF18, 0 0 55px #49FF18, 0 0 75px #49ff18",
+        'text-glow':
+          '0 0 5px #FFF, 0 0 10px #FFF, 0 0 15px #FFF, 0 0 20px #49ff18, 0 0 30px #49FF18, 0 0 40px #49FF18, 0 0 55px #49FF18, 0 0 75px #49ff18',
       },
       animation: {
-        "text-gradient": "gradientMove 4s ease infinite",
+        'text-gradient': 'gradientMove 4s ease infinite',
       },
       keyframes: {
         gradientMove: {
-          "0%, 100%": { backgroundPosition: "0% 50%" },
-          "50%": { backgroundPosition: "100% 50%" },
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
         },
       },
     },
     fontFamily: {
-      body: ["Josefin Sans"],
+      body: ['Josefin Sans'],
       special: ['"Nunito"'],
     },
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
