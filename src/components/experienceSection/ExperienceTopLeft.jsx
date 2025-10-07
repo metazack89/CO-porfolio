43efa9bd4393c1fa327f78f5d7d4e781d07c023c
Chris <cristianortizventas@gmail.com>
2025-10-06 22:22:22 -0500

    change components

--- src/components/experienceSection/ExperienceTopLeft.jsx
+++ src/components/experienceSection/ExperienceTopLeft.jsx
@@ -1,5 +1,5 @@
-import { useTranslation } from "react-i18next";
-import ExperienceInfo from "./ExperienceInfo";
+import { useTranslation } from 'react-i18next';
+import ExperienceInfo from './ExperienceInfo';
 
 const ExperienceTopLeft = () => {
   const { t } = useTranslation();
@@ -7,14 +7,14 @@ const ExperienceTopLeft = () => {
   return (
     <div className="flex flex-col gap-6 w-[300px]">
       <p className="text-orange font-bold uppercase text-3xl font-special text-center">
-        {t("experienceTopLeft.since")}
+        {t('experienceTopLeft.since')}
       </p>
       <div className="flex justify-center items-center gap-4">
-        <ExperienceInfo number="1" text={t("experienceTopLeft.year")} />
+        <ExperienceInfo number="2" text={t('experienceTopLeft.year')} />
         <p className="font-bold text-6xl text-lightBrown">-</p>
-        <ExperienceInfo number="5" text={t("experienceTopLeft.websites")} />
+        <ExperienceInfo number="8" text={t('experienceTopLeft.websites')} />
       </div>
-      <p className="text-center">{t("experienceTopLeft.description")}</p>
+      <p className="text-center">{t('experienceTopLeft.description')}</p>
     </div>
   );
 };
